fc1801df69dcbfe499e2c7612c9317c09ae2b089
Dhruv251004 <118115413+Dhruv251004@users.noreply.github.com>
2024-12-14 17:43:24 +0530

    dep

--- backend/chessBackend/chessBackend/settings.py
+++ backend/chessBackend/chessBackend/settings.py
@@ -41,10 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'game.apps.GameConfig',
-    'users.apps.UsersConfig',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
+    'users.apps.UsersConfig',
+    'game.apps.GameConfig',
 ]
 
 MIDDLEWARE = [
@@ -98,12 +98,6 @@ ASGI_APPLICATION = 'chessBackend.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -158,4 +152,11 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels.layers.InMemoryChannelLayer',
     },
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
