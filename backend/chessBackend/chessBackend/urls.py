21594520f15868932f9f1b90daeec839adc70229
Dhruv251004 <118115413+Dhruv251004@users.noreply.github.com>
2024-12-14 18:21:51 +0530

    added template

--- backend/chessBackend/chessBackend/urls.py
+++ backend/chessBackend/chessBackend/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView
+# Import TemplateView for a simple root view
+from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('users.urls')),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # Add a simple view for the root URL
+    # Serving a basic index page
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
