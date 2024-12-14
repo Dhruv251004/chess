0552ab96c17237a8444bc06fa8e7628f3d6ee5bc
Dhruv251004 <118115413+Dhruv251004@users.noreply.github.com>
2024-12-14 18:09:34 +0530

    chng

--- backend/chessBackend/chessBackend/asgi.py
+++ backend/chessBackend/chessBackend/asgi.py
@@ -1,18 +1,20 @@
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chessBackend.settings')
 import django
 from django.core.asgi import get_asgi_application
+# Get the ASGI application
+django_asgi_app = get_asgi_application()
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from game import routing
 from game.middleware import JWTAuthMiddleware
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chessBackend.settings')
 
 # Make sure Django is properly set up
 django.setup()
 
-# Now get the ASGI application
-django_asgi_app = get_asgi_application()
+# Now import routing after setting up Django
+from game import routing
+
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
