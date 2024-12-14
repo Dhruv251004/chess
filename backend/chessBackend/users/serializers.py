b143de33472e8de721ceecc98fdaabd4db45f852
Dhruv251004 <118115413+Dhruv251004@users.noreply.github.com>
2024-12-14 18:01:10 +0530

    sql

--- backend/chessBackend/users/serializers.py
+++ backend/chessBackend/users/serializers.py
@@ -1,7 +1,6 @@
-from rest_framework.serializers import ModelSerializer
-from django.contrib.auth.models import User
+from rest_framework import serializers
 
-class UserSerializer(ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model=User
+        model = 'django.contrib.auth.models.User'  # Reference by string
         fields = ['first_name', 'last_name', 'email', 'username']
